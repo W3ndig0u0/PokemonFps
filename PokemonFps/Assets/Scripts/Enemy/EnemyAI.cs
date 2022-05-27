c497b2d19b7e94fedafc1960d58b2c57562e63f0
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2022-05-27 16:25:21 +0200

    sdad

---
+++ PokemonFps/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+  public NavMeshAgent agent;
+  public Transform player;
+  public LayerMask whatIsGround;
+
+  void Awake()
+  {
+    player = GameObject.Find("Player").transform;
+    agent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    agent.SetDestination(player.position);
+    // agent.LookAt(player.rotation);
+  }
+
+}
